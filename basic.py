ded4116621aa1a84adbeb3cfa5969d88a306eae6
awi_chauhan <monasingh8980@gmail.com>
2026-07-05 21:30:38 +0530

    basicdef

--- basic.py
+++ basic.py
@@ -3,3 +3,12 @@ def main():
 
 main()
 main()
+
+# high level language & multi paradigm:
+# object oriented: uses object
+# functional programming: functions
+# structured programming: structures
+# procedural oriented: procedures
+
+
+
