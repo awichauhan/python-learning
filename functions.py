e182e2f1766898a50f9aa5d084724c4eefa84cd3
awi_chauhan <monasingh8980@gmail.com>
2026-07-06 13:03:25 +0530

    parameters and arguments

--- functions.py
+++ functions.py
@@ -1,11 +1,11 @@
-def myFunction(x,y):  # definition
+def myFunction(x,y):  # definition;
     c = x + y  # indentation has to be followed
     print(c)  # no need actually after using return
     return c   # for returning output via function calling to some other file or variable
 
 # myFunction(10,20) # function calling
 
-result = myFunction(8,9)
+result = myFunction(8,9) # function calling with parameters/arguments
 
 print("my result is ", result)
 
