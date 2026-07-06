4c6c1552787fe2d720207afd239df2e6cc52d264
awi_chauhan <monasingh8980@gmail.com>
2026-07-06 13:01:33 +0530

    basics of function and function calling, return function

---
+++ functions.py
@@ -0,0 +1,12 @@
+def myFunction(x,y):  # definition
+    c = x + y  # indentation has to be followed
+    print(c)  # no need actually after using return
+    return c   # for returning output via function calling to some other file or variable
+
+# myFunction(10,20) # function calling
+
+result = myFunction(8,9)
+
+print("my result is ", result)
+
+#need to understand concept of return
