b1ecf6cc1f2c0753d00a2c93b461dd647df19d1a
awi_chauhan <monasingh8980@gmail.com>
2026-07-06 19:11:33 +0530

    while loop, nested while loop

---
+++ whileLoop.py
@@ -0,0 +1,24 @@
+def whileLoop():
+
+    loop_number = 1
+
+    while loop_number <= 10:
+        print("Hello World")
+        loop_number += 1
+
+whileLoop()
+def nestedLoop():
+    i = 1
+    j = 1
+    while i <= 5:
+        print("Awantika ", end = "")
+
+        while j <= 4:
+            print("Chauhan ", end = "")
+            j += 1
+
+        i += 1
+        print()
+    print(i)
+
+nestedLoop()
